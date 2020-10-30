423a99a0f47a61055c8d1c2b26a2e30f0d4a4f66
venkata <narayana.1101@gmail.com>
2020-10-29 20:40:46 -0500

    Update API interface

--- app/src/main/java/com/example/leftoverkiller/network/LeftoverKillerInterface.java
+++ app/src/main/java/com/example/leftoverkiller/network/LeftoverKillerInterface.java
@@ -1,7 +1,9 @@
 package com.example.leftoverkiller.network;
 
 import com.example.leftoverkiller.model.Ingredient;
+import com.example.leftoverkiller.model.IngredientListReponse;
 import com.example.leftoverkiller.model.Recipe;
+import com.example.leftoverkiller.model.RecipeListResponse;
 
 import retrofit2.Call;
 import retrofit2.http.FormUrlEncoded;
@@ -11,10 +13,10 @@ public interface LeftoverKillerInterface {
 
     @FormUrlEncoded
     @POST("get_recipes.php")
-    Call<Recipe> getRecipes();
+    Call<RecipeListResponse> getRecipes();
 
     @FormUrlEncoded
     @POST("get_ingredients.php")
-    Call<Ingredient> getIngredients();
+    Call<IngredientListReponse> getIngredients();
 
 }
